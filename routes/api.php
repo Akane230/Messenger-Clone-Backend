5dab139a63a0acf909ae8fc49724467433da5a73
Isabella Gonzales <gonzales.isabella1414@gmail.com>
2026-01-12 01:41:12 +0800

    Add protected /ping endpoint to test auth middleware

--- routes/api.php
+++ routes/api.php
@@ -6,11 +6,12 @@ use Illuminate\Support\Facades\Route;
 
 // Public auth routes
 Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login'])/*->middleware('throttle:5,1'). I removed it for testing purposes*/;
+Route::post('/login', [AuthController::class, 'login']);
 
 // Protected auth routes
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/refresh', [AuthController::class, 'refresh']);
     Route::get('/user', [AuthController::class, 'user']);
+    Route::get('/ping', function () { return response()->json(['ok' => true]); });
 });
